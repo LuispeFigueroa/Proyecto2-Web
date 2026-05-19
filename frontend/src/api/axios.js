ed75fec558d2734591361e02e0cff2d584735233
LuispeFigueroa <fig24087@uvg.edu.gt>
2026-05-18 23:10:12 -0600

    ajustar API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://proyecto2-web-production.up.railway.app',
+    baseURL: 'https://proyecto2-web-production.up.railway.app',
 })
 
 export default api
